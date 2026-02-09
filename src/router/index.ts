2fe3bed364e85335a1057d7e3b42527074b31108
obult <oswin@pixelcorp.nl>
2026-02-09 22:59:23 +0100

    fix titles

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,20 +5,20 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/admin',
-      name: 'admin',
-      component: () => import('@/views/AdminView.vue'),
+		path: '/admin',
+		name: 'admin',
+		component: () => import('@/views/AdminView.vue'),
     },
     {
-      path: '/admin/editor',
-      name: 'editor',
-      component: () => import('@/views/EditorView.vue'),
+		path: '/admin/editor',
+		name: 'editor',
+		component: () => import('@/views/EditorView.vue'),
     },
     {
 		path: '/admin/images',
 		name: 'gallery',
 		component: () => import('@/views/ImageGalleryView.vue'),
-	  },
+	},
     {
       path: '/:pathMatch(.*)*',
       name: 'renderer', // need to make this nicer, maybe configurable or get from markdown
@@ -26,6 +26,16 @@ const router = createRouter({
     }
   ],
 
+})
+
+router.afterEach((to) => {
+  if (to.path === '/') {
+	document.title = 'home';
+  if (to.name === 'renderer') {
+    document.title = to.path.split('/').pop() || 'OsPress';
+  } else {
+	document.title = to.name as string;
+  }
 })
 
 export default router
